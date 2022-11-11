c97f37a0ff3c80972f936dcb280bbf8801b2944d
Dominik Kamiński <d.kaminski@zoho.com>
2022-11-11 11:39:38 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,36 @@
+let amountElement = document.querySelector(".js-form__amount");
+let currencyElement = document.querySelector(".js-form__currency");
+let formElement = document.querySelector(".js-form");
+let resultTextElement = document.querySelector(".js-form__result");
+
+let rateEUR = 4.7146;
+let rateUSD = 4.7328;
+let reteGBP = 5.3795;
+let rateCHF = 4.7905;
+
+console.log("Witaj na mojej stronie Kalulatora walut! Pamiętaj by upewnić się z kiedy są podane kursy walut!")
+
+formElement.addEventListener("submit", (event) => {
+    event.preventDefault();
+    let amount = +amountElement.value;
+    let currenncy = currencyElement.value;
+
+    let result;
+
+    switch (currenncy) {
+        case "EUR":
+            result = amount / rateEUR;
+            break;
+        case "USD":
+            result = amount / rateUSD;
+            break;
+        case "GBP":
+            result = amount / reteGBP;
+            break;
+        case "CHF":
+            result = amount / rateCHF;
+            break;
+    }
+
+    resultTextElement.innerText = result.toFixed(2) + " " + currenncy;
+})
